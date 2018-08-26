a3e3bb390a3cd98f8c72a2223c9cf2b3929a3b45
Brian Hernandez <brianhernandez@users.noreply.github.com>
2018-08-26 08:49:54 -0400

    Basic app functionality complete.

--- app.js
+++ app.js
@@ -1,3 +1,45 @@
 $( document ).ready(function() {
-    console.log( "ready!" );
+    $moduleElement = $('.module');
+
+    $moduleElement.on('click', function(event) {
+      colorButton = event.target;
+
+      if (colorButton.classList.contains('module__button--red')) {
+        toggleColor('red');
+      } else if (colorButton.classList.contains('module__button--green')) {
+        toggleColor('green');
+      } else if (colorButton.classList.contains('module__button--blue')) {
+        toggleColor('blue');
+      } else if (colorButton.classList.contains('module__button--white')) {
+        toggleColor('white');
+      }
+    });
+
+    function toggleColor(color) {
+      $nonWhiteElements = $('.module').children().not('.module__box--white');
+
+      if (color !== 'white') {
+        switch (color) {
+          case 'red':
+            $nonWhiteElements.removeClass('module__box--green module__box--blue')
+              .addClass('module__box--red');
+            break;
+          case 'green':
+            $nonWhiteElements.removeClass('module__box--red module__box--blue')
+              .addClass('module__box--green');
+            break;
+          case 'blue':
+            $nonWhiteElements.removeClass('module__box--red module__box--green')
+              .addClass('module__box--blue');
+            break;
+        }
+      } else {
+        $($nonWhiteElements[0]).removeClass('module__box--green module__box--blue')
+          .addClass('module__box--red');
+        $($nonWhiteElements[1]).removeClass('module__box--red module__box--blue')
+          .addClass('module__box--green');
+        $($nonWhiteElements[2]).removeClass('module__box--red module__box--green')
+          .addClass('module__box--blue');
+      }
+    };
 });
